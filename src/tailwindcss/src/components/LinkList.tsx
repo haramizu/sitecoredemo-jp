a9d6fd7a905a368b2ece1180ca9037ae45d09365
Shinichi Haramizu <shinichi@haramizu.jp>
2024-09-12 11:32:55 +0900

    add locale to linklist

--- src/tailwindcss/src/components/LinkList.tsx
+++ src/tailwindcss/src/components/LinkList.tsx
@@ -53,7 +53,7 @@ const LinkListItem = (props: LinkListItemProps) => {
   return (
     <li className={className}>
       <div className="field-link">
-        <JssLink field={props.field} />
+        <JssLink field={props.field} href={`/${props.locale}${props.field.value.href}`} />
       </div>
     </li>
   );
