0e2118984a04a902ec3c3a34f807c7ed6c21408f
Shinichi Haramizu <shinichi@haramizu.jp>
2024-09-18 13:41:38 +0900

    update responsive card

--- src/tailwindcss/src/components/ProductList.tsx
+++ src/tailwindcss/src/components/ProductList.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import {
-  ComponentParams,
-  TextField,
-  RichText as JssRichText,
-  Image as JssImage,
-} from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentParams, TextField, Text, Image, SitecoreContextValue, useSitecoreContext } from '@sitecore-jss/sitecore-jss-nextjs';
+import Link from 'next/link';
 
 interface ProductField {
   id: string;
@@ -15,7 +11,12 @@ interface ProductField {
   name: string;
   value: string;
 }
+
 interface ProductProps {
+  url: {
+    path: string;
+    siteName: string;
+  };
   field: {
     id: string;
     jsonValue: {
@@ -47,19 +48,38 @@ interface ProductListProps {
   fields: Fields;
 }
 
+const getLocale = function (props: SitecoreContextValue): string {
+  let locale;
+
+  if (!props.language || props.language === process.env.DEFAULT_LANGUAGE) {
+    locale = '';
+  } else {
+    locale = '/' + props.language;
+  }
+
+  return locale;
+};
+
 export const Default = (props: ProductListProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
   const styles = `component product-list ${props.params.styles}`.trimEnd();
+  const { sitecoreContext } = useSitecoreContext();
+
+  const contentLocale = getLocale(sitecoreContext);
 
   const datasource = props.fields?.data?.datasource;
 
   if (datasource) {
-    const children = ProductList(datasource.children.results);
+    const children = ProductList(datasource.children.results, contentLocale);
 
     return (
       <div className={styles} id={id ? id : undefined}>
         <div className="component-content">
-          <>{children}</>
+          <div className="container mx-auto p-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+              <>{children}</>
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -74,7 +94,7 @@ export const Default = (props: ProductListProps): JSX.Element => {
   );
 };
 
-const ProductList = (props: ProductProps[]) => {
+const ProductList = (props: ProductProps[], locale: string) => {
   const children: JSX.Element[] = [];
   let titleContent: JSX.Element | null = null;
   let imageContent: JSX.Element | null = null;
@@ -82,16 +102,16 @@ const ProductList = (props: ProductProps[]) => {
   for (const product of props) {
     for (const field of product.fields) {
       if (field.name === 'NavigationTitle') {
-        titleContent = <JssRichText field={field.jsonValue} />;
+        titleContent = <Text field={field.jsonValue} />;
       }
       if (field.name === 'Image') {
-        imageContent = <JssImage field={field.jsonValue} />;
+        imageContent = <Image field={field.jsonValue} className="w-full h-full object-contain" />;
       }
     }
     children.push(
-      <div>
-        {titleContent}
-        {imageContent}
+      <div className="card border rounded shadow min-h-[250px]">
+        <div className="aspect-w-16 aspect-h-9">{imageContent}</div>
+        <div className="ml-8 mt-5 text-xl"><Link href={locale + product.url.path}>{titleContent}</Link></div>
       </div>
     );
   }
